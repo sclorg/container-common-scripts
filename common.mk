c7a0cfe60da041da1d722743914294189ff3d66e
Petr Kubat <pkubat@redhat.com>
2017-06-29 04:57:26 -0400

    Do not use VERSION and VERSIONS for the same purpose in common.mk
    Instead only use VERSIONS to choose which versions of the image to build in the makefile
    and depend on its rules for populating the VERSION variable to be used in build.sh

--- common.mk
+++ common.mk
@@ -17,26 +17,24 @@ endif
 script_env = \
 	SKIP_SQUASH=$(SKIP_SQUASH)                      \
 	UPDATE_BASE=$(UPDATE_BASE)                      \
-	VERSIONS="$(VERSIONS)"                          \
 	OS=$(OS)                                        \
-	VERSION="$(VERSION)"                            \
 	OPENSHIFT_NAMESPACES="$(OPENSHIFT_NAMESPACES)"
 
 .PHONY: build
-build: manpages
-	$(script_env) $(build)
+build: $(VERSIONS)
+
+.PHONY: $(VERSIONS)
+$(VERSIONS): % : %/root/help.1
+	VERSION="$@" $(script_env) $(build)
 
 .PHONY: test
-test: manpages
-	$(script_env) TAG_ON_SUCCESS=$(TAG_ON_SUCCESS) TEST_MODE=true $(build)
+test: script_env += TEST_MODE=true
+test: $(VERSIONS)
 
 .PHONY: test-openshift
-test-openshift: manpages
-	$(script_env) TAG_ON_SUCCESS=$(TAG_ON_SUCCESS) TEST_OPENSHIFT_MODE=true $(build)
+test-openshift: script_env += TEST_OPENSHIFT_MODE=true
+test-openshift: $(VERSIONS)
 
-manpages = $(shell for version in $(if $(VERSION), $(VERSION), $(VERSIONS)); \
-		do echo "$$version/root/help.1"; done)
-manpages: $(manpages)
-$(manpages): %root/help.1: %README.md
+%root/help.1: %README.md
 	mkdir -p $(@D)
 	go-md2man -in "$^" -out "$@"
