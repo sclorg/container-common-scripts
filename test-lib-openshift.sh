bda9c033aa53c84eaedf7bbed73784da12e63824
Honza Horak <hhorak@redhat.com>
2018-10-31 08:22:39 +0100

    Extract just the image name for internal cmd image

--- test-lib-openshift.sh
+++ test-lib-openshift.sh
@@ -938,7 +938,7 @@ function ct_os_check_cmd_internal() {
   local ip=$(ct_os_get_service_ip "${service_name}")
   local check_command_exp=$(echo "$check_command" | sed -e "s/<IP>/$ip/g")
 
-  ct_os_deploy_cmd_image $(ct_os_get_image_from_pod "${util_image_name}" | head -n 1)
+  ct_os_deploy_cmd_image $(ct_os_get_image_from_pod "${util_image_name##*/}" | head -n 1)
   SECONDS=0
 
   echo -n "Waiting for ${service_name} service becoming ready ..."
