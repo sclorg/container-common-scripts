d864f8cd9ab9e7048dad43de782c5ff8c357ff70
Honza Horak <hhorak@redhat.com>
2019-12-13 17:52:38 +0100

    Fix many shellcheck errors in clean.sh script

--- clean.sh
+++ clean.sh
@@ -7,10 +7,12 @@ for version
 do
     remove_images=
     for idfile in .image-id.raw .image-id.squashed; do
+        # shellcheck disable=SC2039
         test ! -f "$version/$idfile" || remove_images+=" $(cat "$version/$idfile")"
     done
 
     for image in $remove_images; do
+        # shellcheck disable=SC2046
         docker rm -f $(docker ps -q -a -f "ancestor=$image") 2>/dev/null || :
         docker rmi -f "$image" || :
     done
