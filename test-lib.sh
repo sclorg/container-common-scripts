9361dec7c0abb04f40f80dbda49b472e7bc68e44
Honza Horak <hhorak@redhat.com>
2020-11-10 11:29:18 +0100

    Let cleanup continue if the entry is not a regular file

--- test-lib.sh
+++ test-lib.sh
@@ -27,7 +27,7 @@ EXPECTED_EXIT_CODE=0
 function ct_cleanup() {
   ct_show_resources
   for cid_file in "$CID_FILE_DIR"/* ; do
-    [ -f "$cid_file" ] || break
+    [ -f "$cid_file" ] || continue
     local container
     container=$(cat "$cid_file")
 
