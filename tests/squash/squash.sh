f9f79fa7a017d5162e4d3e64ac976322e809aecc
Petr Hracek <phracek@redhat.com>
2021-05-17 11:34:08 +0200

    Update test-lib-openshift.sh

--- tests/squash/squash.sh
+++ tests/squash/squash.sh
@@ -8,7 +8,7 @@ if grep -q "Red Hat Enterprise Linux release 8" /etc/system-release; then
   exit 0
 fi
 
-origin=quay.io/centos7/s2i-core-centos7
+origin=quay.io/quay/busybox
 squash=$(dirname "$(readlink -f "$0")")/../../squash.py
 cd "$(dirname "$0")"
 
