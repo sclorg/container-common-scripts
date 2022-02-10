beca699792849b376c54e6b9bbd5aaca57bb8d4d
Petr "Stone" Hracek <phracek@redhat.com>
2022-02-10 10:28:29 +0100

    Fix shellchecks issues
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test-lib.sh
+++ test-lib.sh
@@ -125,7 +125,7 @@ function ct_check_envs_set {
     for value in $stripped; do
         # If the falue checked does not go through env_filter we do not care about it
         echo "$value" | grep -q "$env_filter" || continue
-        if [ -n "${filtered_envs##${env_format//VALUE/$value}}" ]; then
+        if [ -n "${filtered_envs##"${env_format//VALUE/$value}"}" ]; then
             echo " Value $value is missing from variable $var_name"
             echo "$filtered_envs"
             IFS=$old_IFS
@@ -1066,7 +1066,7 @@ ct_check_testcase_result() {
     echo "Test for image '${IMAGE_NAME}' FAILED (exit code: ${result})"
     TESTCASE_RESULT=1
   fi
-  return $result
+  return "$result"
 }
 
 # ct_run_tests_from_testset
