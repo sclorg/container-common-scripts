d53615ea12b91e604ee7eb6d6e1a6ee71ec12880
Zuzana Miklankova <zmiklank@redhat.com>
2022-08-02 12:30:09 +0200

    remove squash exception for RHELs

--- tests/failures/check/check_skip_squash.sh
+++ tests/failures/check/check_skip_squash.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-set -e
-
-if grep -q "Red Hat Enterprise Linux release 8" /etc/system-release || grep -q "Red Hat Enterprise Linux release 9" /etc/system-release ; then
-  echo "make tag SKIP_SQUASH=0 is skipped on RHEL8 and RHEL 9."
-  exit 0
-fi
-
 make tag SKIP_SQUASH=0
