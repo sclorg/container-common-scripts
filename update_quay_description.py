10d0993c5473ad1b3f6c26d27c89c7ec6f5956d7
Adam Dobeš <adobes@redhat.com>
2023-04-04 12:55:02 +0200

    Add function for loading README

--- update_quay_description.py
+++ update_quay_description.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import os
 
 from typing import List
 
@@ -13,3 +14,16 @@ def get_versions() -> List[str]:
                 line = line.strip()
                 versions = line[len(pattern):].split(" ")
     return versions
+
+
+def load_readme(dir: str) -> str:
+    """
+    Loads repository README starting from (but not including) Description line
+    """
+    
+    readme_path = os.path.join(dir, "README.md")
+    with open(readme_path) as readme:
+        lines = readme.readlines()
+        for i, line in enumerate(lines):
+            if re.match("Description", line):
+                return lines[i + 3:] # Skip seperator line and empty line
