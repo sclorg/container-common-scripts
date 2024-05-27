33d6a609ef026e6f073fef892602db5685bb912b
Petr "Stone" Hracek <phracek@redhat.com>
2024-05-27 16:54:57 +0200

    Show all imagestreams in imagestream file New Python function is created so the imagestreams are shown always.
    This fixes PR https://github.com/sclorg/container-common-scripts/pull/256
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- tests/check_imagestreams.sh
+++ tests/check_imagestreams.sh
@@ -3,7 +3,12 @@
 set -x
 
 check_imagestreams=$(dirname "$(readlink -f "$0")")/../check_imagestreams.py
+show_all_imagestreams=$(dirname "$(readlink -f "$0")")/../show_all_imagestreams.py
 "${PYTHON-python3}" "$check_imagestreams" "2.5"
 test $? -eq 1
 "${PYTHON-python3}" "$check_imagestreams" "2.4"
 test $? -eq 0
+echo "Check full is output"
+output=$("${PYTHON-python3}" "$show_all_imagestreams")
+test "${output#*"- latest -> 2.4"}" != "$output" && echo "latest found in the output"
+test "${output#*"- 2.4 -> registry.redhat.io/rhscl/httpd-24-rhel7"}" != "$output" && echo "2.4 found in the output"
