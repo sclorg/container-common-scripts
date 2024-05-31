1903527ebf5564ecb8ca5b9c155dbeb770a76d08
Petr "Stone" Hracek <phracek@redhat.com>
2024-05-31 09:39:22 +0200

    Update test output text for better understanding
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- tests/check_imagestreams.sh
+++ tests/check_imagestreams.sh
@@ -8,7 +8,8 @@ show_all_imagestreams=$(dirname "$(readlink -f "$0")")/../show_all_imagestreams.
 test $? -eq 1
 "${PYTHON-python3}" "$check_imagestreams" "2.4"
 test $? -eq 0
-echo "Check full is output"
+
+echo "This tests check if 'show_all_imagestreams.py' returns proper output"
 output=$("${PYTHON-python3}" "$show_all_imagestreams")
 test "${output#*"- latest -> 2.4"}" != "$output" && echo "latest found in the output"
 test "${output#*"- 2.4 -> registry.redhat.io/rhscl/httpd-24-rhel7"}" != "$output" && echo "2.4 found in the output"
