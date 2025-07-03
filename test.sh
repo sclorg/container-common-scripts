cb5168b59b11a675cc3d5c87525eacd9a8ca6a34
Petr "Stone" Hracek <phracek@redhat.com>
2025-07-03 15:45:09 +0200

    Store logdetective output to tempfile
    and at the end store it to $TMT_TEST_DATA directory
    so we see it in Testing Farm results
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test.sh
+++ test.sh
@@ -25,6 +25,7 @@ failed_version() {
 }
 
 analyze_logs_by_logdetective() {
+  # logdetective should not break the test functionality
   set +e
   local log_file_name="$1"
   echo "Sending failed log by fpaste command to paste bin."
@@ -39,16 +40,26 @@ analyze_logs_by_logdetective() {
   raw_paste_bin_link="${paste_bin_link//view/view\/raw}"
   echo "Sending log file to logdetective server: ${raw_paste_bin_link}"
   echo "-------- LOGDETECTIVE TEST LOG ANALYSIS START --------"
+  logdetective_test_file=$(mktemp "/tmp/logdetective_test.XXXXXX")
   # shellcheck disable=SC2181
-  if ! curl -k --insecure --header "Content-Type: application/json" --request POST --data "{\"url\":\"${raw_paste_bin_link}\"}" "$LOGDETECTIVE_SERVER/analyze" > /tmp/logdetective_test_output.txt; then
+  if ! curl -k --insecure --header "Content-Type: application/json" --request POST --data "{\"url\":\"${raw_paste_bin_link}\"}" "$LOGDETECTIVE_SERVER/analyze" >> "${logdetective_test_file}"; then
     echo "ERROR: Failed to analyze log file by logdetective server."
-    cat "/tmp/logdetective_test_output.txt"
+    cat "${logdetective_test_file}"
     echo "-------- LOGDETECTIVE TEST LOG ANALYSIS FAILED --------"
     set -e
     return
   fi
-  jq -rC '.explanation.text' < "/tmp/logdetective_test_output.txt"
   set -e
+  jq -rC '.explanation.text' < "${logdetective_test_file}"
+  # This part of code is from https://github.com/teemtee/tmt/blob/main/tmt/steps/scripts/tmt-file-submit
+  if [ -z "$TMT_TEST_PIDFILE" ]; then
+    echo "File submit to data dir can be used only in the context of a running test."
+    return
+  fi
+  # This variable is set by tmt
+  [ -d "$TMT_TEST_DATA" ] || mkdir -p "$TMT_TEST_DATA"
+  cp -f "${logdetective_test_file}" "$TMT_TEST_DATA"
+  echo "File '${logdetective_test_file}' stored to '$TMT_TEST_DATA'."
   echo "-------- LOGDETECTIVE TEST LOG ANALYSIS FINISHED --------"
 }
 
