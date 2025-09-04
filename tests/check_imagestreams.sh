bdd9bcfe803e95020efdd5b76951f97f1f43aa82
Petr "Stone" Hracek <phracek@redhat.com>
2025-09-04 10:45:22 +0200

    Replace RHEL8 -> RHEL10 in check_imagestreams.sh testing_file.json and public_image_name
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- tests/check_imagestreams.sh
+++ tests/check_imagestreams.sh
@@ -12,4 +12,4 @@ test $? -eq 0
 echo "This tests check if 'show_all_imagestreams.py' returns proper output"
 output=$("${PYTHON-python3}" "$show_all_imagestreams")
 test "${output#*"- latest -> 2.4"}" != "$output" && echo "latest found in the output"
-test "${output#*"- 2.4-el8 -> registry.redhat.io/rhel8/httpd-24"}" != "$output" && echo "2.4 found in the output"
+test "${output#*"- 2.4-el10 -> registry.redhat.io/rhel10/httpd-24"}" != "$output" && echo "2.4 found in the output"
